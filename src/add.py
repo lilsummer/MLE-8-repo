547a994dd1aa97e06798f04ee70ecaba0aa44dd6
Tikeswar Naik <tikeswar@gmail.com>
2022-06-25 19:28:24 +0530

    added a convenient method to add 100 to the input

--- src/add.py
+++ src/add.py
@@ -9,3 +9,15 @@ def add(x:int, y:int=100) -> int:
                     int: The sum of the two numbers.
                 """
                 return x + y
+
+def add100(x:int) -> int:
+    '''
+    Add 100 to the number.
+
+    Args:
+        x (int): the number
+
+    Returns:
+        int: the number plus 100
+    '''
+    return x + 100
